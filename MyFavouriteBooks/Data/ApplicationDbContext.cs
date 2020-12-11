4e4a9039f15a2377117b4512619fb9980fc4ce32
Asadullah Qamar Bin Qamar Siddique Bhatti <asadullah@studentambassadors.com>
2020-12-12 07:15:56 +0800

    Add project files.

---
+++ MyFavouriteBooks/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFavouriteBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFavouriteBooks.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ MyFavouriteBooks/Migrations/20201211231117_addBooksToDatabase2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFavouriteBooks.Migrations
+{
+    public partial class addBooksToDatabase2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Filename",
+                table: "Books");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Filename",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
